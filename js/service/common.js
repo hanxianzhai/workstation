6af8710fc4a5f8ade05612658982dad4ff327bb3
tristan-tsl <tanshilinmail@gmail.com>
2020-03-17 21:49:58 +0800

    finish the designer-data-directory

--- js/service/common.js
+++ js/service/common.js
@@ -6,7 +6,8 @@ function init_axios() {
         axios.defaults.crossDomain = true;
     }
 }
-async function do_graphql(request_data){
+
+async function do_execute_sql(request_data) {
     try {
         component.$Loading.start();
         const res_data = await axios.post("/data", request_data);
@@ -14,10 +15,18 @@ async function do_graphql(request_data){
         return res_data;
     } catch (e) {
         component.$Loading.error();
-        e.print();
         throw e;
     }
 
 }
 
 init_axios();
+String.prototype.format = function () {
+    if (arguments.length == 0) return this;
+    const obj = arguments[0];
+    let s = this;
+    for (const key in obj) {
+        s = s.replace(new RegExp("\\{\\{" + key + "\\}\\}", "g"), obj[key]);
+    }
+    return s;
+};
